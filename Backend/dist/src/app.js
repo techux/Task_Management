aaebcac0d6f1b330dd3a937fc8937a5f912b3616
riyasharma <riya.sharma@codingworkx.com>
2025-12-17 08:49:28 +0530

    Added root API message

--- Backend/dist/src/app.js
+++ Backend/dist/src/app.js
@@ -1,39 +1,9 @@
 "use strict";
-// import express, { Application } from "express";
-// import routev1 from "./routes/routev1";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
-// export class App {
-//   public app: Application;
-//   public port: string | number;
-//   public base_url: string;
-//   constructor(port: string | number, base_url: string) {
-//     this.app = express();
-//     this.port = port;
-//     this.base_url = base_url;
-//   }
-//   public async initialize(): Promise<void> {
-//     try {
-//       this.initializeMiddlewares();
-//       this.initializeRoutes();
-//      // console.log("Express app initialized");
-//     } catch (error) {
-//       console.log("Server Initialization error:", error);
-//      // process.exit(1);
-//     }
-//   }
-//   private initializeMiddlewares(): void {
-//     this.app.use(express.json());
-//     this.app.use(express.urlencoded({ extended: true }));
-//     this.app.use("/uploads", express.static("uploads"));
-//   }
-//   private initializeRoutes(): void {
-//     this.app.use("/api/v1", routev1);
-//   }
-// }
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const routev1_1 = __importDefault(require("./routes/routev1"));
@@ -61,6 +31,11 @@ class App {
         this.app.use((0, cookie_parser_1.default)());
     }
     initializeRoutes() {
+        this.app.use('/', (req, res) => {
+            res.status(200).json({
+                message: "Collaborative Task Manager API is running"
+            });
+        });
         this.app.use("/api/v1", routev1_1.default);
     }
 }
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,41 +1,6 @@
-// import express, { Application } from "express";
-// import routev1 from "./routes/routev1";
-
-// export class App {
-//   public app: Application;
-//   public port: string | number;
-//   public base_url: string;
-
-//   constructor(port: string | number, base_url: string) {
-//     this.app = express();
-//     this.port = port;
-//     this.base_url = base_url;
-//   }
-
-//   public async initialize(): Promise<void> {
-//     try {
-//       this.initializeMiddlewares();
-//       this.initializeRoutes();
-//      // console.log("Express app initialized");
-//     } catch (error) {
-//       console.log("Server Initialization error:", error);
-//      // process.exit(1);
-//     }
-//   }
-
-//   private initializeMiddlewares(): void {
-//     this.app.use(express.json());
-//     this.app.use(express.urlencoded({ extended: true }));
-//     this.app.use("/uploads", express.static("uploads"));
-//   }
-
-//   private initializeRoutes(): void {
-//     this.app.use("/api/v1", routev1);
-//   }
-// }
-
 import express, { Application } from "express";
 import cookieParser from "cookie-parser";
+import {Request, Response} from 'express';
 import routev1 from "./routes/routev1";
 
 export class App {
@@ -65,6 +30,12 @@ export class App {
   }
 
   private initializeRoutes(): void {
+    this.app.use('/', (req: Request, res: Response)=>{
+       res.status(200).json({
+        message: "Collaborative Task Manager API is running"
+       })
+       
+    })
     this.app.use("/api/v1", routev1);
   }
 }
