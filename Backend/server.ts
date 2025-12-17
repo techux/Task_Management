8df535da9d1d0bb1a3f39ba5a62198c0458b1c52
riyasharma <riya.sharma@codingworkx.com>
2025-12-17 17:55:26 +0530

    changes

--- Backend/server.ts
+++ Backend/server.ts
@@ -14,14 +14,16 @@ const base_url = process.env.BASE_URL || "";
 const myApp = new App(port, base_url);
 const app = myApp.app;
 
-app.use(cors({
+app.use(
+  cors({
+    origin: [
+      "https://task-management-blue-ten.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
-  origin: [
-    "task-management-blue-ten.vercel.app",
-  ],
-
-  credentials: true,
-}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
